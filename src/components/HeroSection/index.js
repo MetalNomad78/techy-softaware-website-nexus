e83b2af08b1b16706f6c17b3beaa6304f0ae690b
Siddhant Singh <singhsiddhant9760@gmail.com>
2023-12-28 17:21:00 +0530

    new animation of fading in

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,45 +1,67 @@
-import React from 'react'
-import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.png'
+import React from 'react';
+import HeroBgAnimation from '../HeroBgAnimation';
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  SocialMediaIcons,
+  SocialMediaIcon,
+  ResumeButton,
+} from './HeroStyle';
+import HeroImg from '../../images/HeroImage.png';
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
+import './HeroSection.css'; // Import your CSS file
 
 const HeroSection = () => {
-    return (
-        <div id="about">
-            <HeroContainer>
-                <HeroBg>
-                    <HeroBgAnimation />
-                </HeroBg>
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>Hi, I am <br /> {Bio.name}</Title>
-                        <TextLoop>
-                            I am a
-                            <Span>
-                                <Typewriter
-                                    options={{
-                                        strings: Bio.roles,
-                                        autoStart: true,
-                                        loop: true,
-                                    }}
-                                />
-                            </Span>
-                        </TextLoop>
-                        <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Resume/CV</ResumeButton>
-                    </HeroLeftContainer>
+  const nameStyle = {
+    color: '#FFD700', // Set the color to gold/yellow
+  };
 
-                    <HeroRightContainer id="Right">
+  return (
+    <div id="about">
+      <HeroContainer className="hero-container"> {/* Add a class for styling */}
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer className="hero-inner-container"> {/* Add a class for styling */}
+          <HeroLeftContainer id="Left" className="hero-left-container"> {/* Add a class for styling */}
+            <Title className="fade-in">
+              Hi, I am <br />
+            </Title>
+            <Title style={nameStyle} className="fade-in">{Bio.name}</Title>
+            <TextLoop className="fade-in">
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle className="fade-in">{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display" className="fade-in">
+              Resume/CV
+            </ResumeButton>
+          </HeroLeftContainer>
 
-                        <Img src={HeroImg} alt="hero-image" />
-                    </HeroRightContainer>
-                </HeroInnerContainer>
+          <HeroRightContainer id="Right" className="hero-right-container"> {/* Add a class for styling */}
+            <Img src={HeroImg} alt="hero-image" className="fade-in" /> {/* Add a class for styling */}
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
 
-            </HeroContainer>
-        </div>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
